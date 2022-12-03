4614d22e92c59145323b5d16cffda5c993a489cd
cintiasilva7300 <cintiasilva7300@gmail.com>
2022-12-02 23:11:53 -0300

    first commit

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,19 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema()
+export class User {
+  @Prop()
+  name: string;
+
+  @Prop()
+  email: string;
+
+  @Prop()
+  password: string;
+}
+
+// eslint-disable-next-line prettier/prettier
+export const UserSchema = SchemaFactory.createForClass(User);
